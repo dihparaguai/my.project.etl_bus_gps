e686c1028ce1254973e02244649f51fde6cd0766
Diego Paraguai <diego.paraguai.dp@gmail.com>
2025-06-26 13:35:27 -0300

    feat: add method to upload silver files to GCP

--- scripts_class/gpc_uploader.py
+++ scripts_class/gpc_uploader.py
@@ -6,12 +6,21 @@ class GCPUploader():
         self.bucket_name = bucket_name
         self.client = storage.Client() # Cria o cliente para acessar o bucket
         self.bucket = self.client.bucket('etl_bus_gps') # Referência pro bucket do GCP
-        pass
 
-    def upload_to_bucket_bronze(self, path_bronze, file_bronze): # Faz o upload de todos os arquivos para o bucket
-        print('Iniciando upload dos arquivos para a Cloud.')
-        file_path = os.path.join(path_bronze, file_bronze)
+    def upload_to_bucket_bronze(self, path_bronze, file_name_bronze): # Faz o upload de todos os arquivos para o bucket
+        print('Iniciando upload do arquivo para a Cloud.')
+        path_folder_file_bronze = os.path.join(path_bronze, file_name_bronze) # Caminho completo do arquivo a ser enviado
 
-        blob = self.bucket.blob(f'bronze/{file_bronze}')
-        blob.upload_from_filename(file_path)
-        print(f'Arquivo "{file_bronze}" enviado para a pasta "/bronze" no bucket "{self.bucket.name}"')
+        blob = self.bucket.blob(f'bronze/{file_name_bronze}') # Cria o blob no bucket
+        blob.upload_from_filename(path_folder_file_bronze) # Faz o upload do arquivo para o bucket
+        print(f'Arquivo "{file_name_bronze}" enviado para a pasta "/bronze" no bucket "{self.bucket.name}"')
+
+    def upload_to_bucket_silver(self, path_folder_silver, file_name_silver): # Função para enviar o arquivo limpo para o bucket do GCP
+        print('Iniciando upload do Arquivo para a Cloud.')
+        
+        path_folder_file_silver = os.path.join(path_folder_silver, file_name_silver) # Caminho completo do arquivo a ser enviado
+        
+        blob = self.bucket.blob(f'silver/{file_name_silver}') # Cria o blob no bucket
+        blob.upload_from_filename(path_folder_file_silver) # Faz o upload do arquivo para o bucket
+
+        print(f'Arquivo "{file_name_silver}" enviado para a pasta "silver/" no bucket "{self.bucket.name}"')
