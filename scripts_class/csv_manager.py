eb15110e1bad58b3bd7bfb7f66b19d4b0d45e9a7
Diego Paraguai <diego.paraguai.dp@gmail.com>
2025-06-26 13:38:59 -0300

    feat: implement CSVManager class for reading bronze and saving silver CSV files

---
+++ scripts_class/csv_manager.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+class CSVMananger():
+    def __init__(self):
+        pass
+
+    def read_csv_bronze(self, path_folder_bronze, file_name_bronze):
+        print('Iniciciando leitura do CSV')
+
+        # Devido a limitação de memória, o arquivo CSV é lido em pedaços (chunks)
+        df_chunk = pd.read_csv(os.path.join(path_folder_bronze, file_name_bronze), on_bad_lines='skip', chunksize=10000)
+
+        print(f'Arquivo "{file_name_bronze}" lido.')
+        return df_chunk
+
+    def save_to_csv_silver(self, df, path_folder_silver, file_name_silver): # Função para salvar o DataFrame limpo no diretório silver
+        print('Iniciciando leitura do CSV')
+
+        df.to_csv(os.path.join(path_folder_silver, file_name_silver), index=False) # Salva o DataFrame limpo no diretório silver
+
+        print(f'Arquivo "{file_name_silver}" salvo no diretório "{path_folder_silver}".')
