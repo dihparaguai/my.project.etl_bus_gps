4ec97e1b06fa642a440ae2d76231b3ad494bc629
Diego Paraguai <diego.paraguai.dp@gmail.com>
2025-07-10 20:11:26 -0300

    refactor: update parameter name for clarity in KaggleDownloader.download_kaggle_dataset method

--- scripts_class/kaggle_downloader.py
+++ scripts_class/kaggle_downloader.py
@@ -6,13 +6,13 @@ class KaggleDownloader:
         self.kaggle_owner = kaggle_owner
         self.kaggle_dataset = kaggle_dataset
 
-    def download_kaggle_dataset(self, path_bronze):
-        print('Iniciando download dos arquivos do Kaggle')
+    def download_kaggle_dataset(self, folder_path_bronze):
+        print('-> Iniciando download dos arquivos do Kaggle.')
         path_origin = kagglehub.dataset_download(os.path.join(self.kaggle_owner, self.kaggle_dataset)) # Baixa os arquivos de GPS do Kaggle
-        os.system(f'mkdir -p {path_bronze}') # Cria a para que conterá os arquivos originais baixados
+        os.system(f'mkdir -p {folder_path_bronze}') # Cria a para que conterá os arquivos originais baixados
 
         # Copia todos os arquivos da pasta do Kaggle para data_raw
-        os.system(f'mv {path_origin}/* {path_bronze}/')
+        os.system(f'mv {path_origin}/* {folder_path_bronze}/')
         os.system(f'rm -rf {path_origin}')
         
-        print(f'Arquivos baixados e movidos para a pasta {path_bronze}.')
+        print(f'Arquivos baixados e movidos para a pasta {folder_path_bronze}.')
